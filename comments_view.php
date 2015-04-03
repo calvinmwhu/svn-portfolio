b47bb236bc1bdf8ef46e29f5a02cbcde793b18f6
Mingwei Hu <calvinmwhu@gmail.com>
2015-04-03 06:11:22 -0500

    comment

--- comments_view.php
+++ comments_view.php
@@ -1,7 +1,27 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: calvinmwhu
- * Date: 3/31/15
- * Time: 10:51 PM
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Portfolio</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="www/inc/css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
+
+
+<body>
+<?php include_once 'lib/header.php'; ?>
+
+<div class="container">
+    <div class="page-header">
+        <h1>Comments Area</h1>
+    </div>
+    <p class="lead">Leave me a comment if you have any suggestions!</p>
+</div>
+
+
+<?php include_once 'lib/footer.php'; ?>
+
+</body>
